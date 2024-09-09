5954588f1ad0c747ea6977b89b4dbb929bf1018f
Aniket Devmore <aniketdevmore1008@gmail.com>
2024-09-09 23:31:51 +0530

    Added Vector Icons

--- App.tsx
+++ App.tsx
@@ -2,28 +2,62 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import {
-  Image,
-  StatusBar,
-  StyleSheet,
-} from 'react-native';
 import ManageExpense from './src/screens/ManageExpense';
 import RecentExpense from './src/screens/RecentExpense';
 import AllExpense from './src/screens/AllExpense';
 import { colors } from './src/assets/Colors';
-// import calendar from '';
+import Icon, { IconType } from "react-native-dynamic-vector-icons";
 
 const Stack = createStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 const ExpenseOverviw = () => {
+  const renderTabIcon = (
+    route: any,
+    focused: boolean,
+    color: string,
+    size: number,
+  ) => {
+    let iconName = "hour-glass";
+    let iconType = IconType.Ionicons
+    switch (route.name) {
+      case "RecentExpense":
+        iconName = focused ? "hourglass" : "hourglass-outline";
+        break;
+      case "AllExpense":
+        iconName = focused ? "calendar" : "calendar-outline";
+        break;
+      default:
+        iconName = focused ? "hourglass" : "hourglass-outline";
+        break;
+    }
+    return (
+      <Icon
+        name={iconName}
+        type={iconType}
+        size={size}
+        color={color}
+      />
+    );
+  };
   return (
-    <BottomTabs.Navigator screenOptions={{
-      headerStyle: {backgroundColor: colors.primary500},
-      headerTintColor: '#ffffff',
+    <BottomTabs.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) =>
+        renderTabIcon(route, focused, color, size),
+      tabBarActiveTintColor: colors.accent500,
+      tabBarInactiveTintColor: "#a6a6a6",
       tabBarStyle: {backgroundColor: colors.primary500},
-      tabBarActiveTintColor: colors.accent500
-    }}>
+      headerTintColor: '#ffffff',
+      headerStyle: {backgroundColor: colors.primary500},
+    })}
+    //  screenOptions={{
+    //   headerStyle: {backgroundColor: colors.primary500},
+    //   headerTintColor: '#ffffff',
+    //   tabBarStyle: {backgroundColor: colors.primary500},
+    //   tabBarActiveTintColor: colors.accent500
+    // }}
+    >
       <BottomTabs.Screen
        name='RecentExpense' 
        component={RecentExpense}
@@ -55,7 +89,7 @@ const App = (): React.JSX.Element => {
           <Stack.Screen
             name='ExpenseOverview' 
             component={ExpenseOverviw} 
-            options={{ headerShown: false }} 
+            options={{ headerShown: false }}
             />
           <Stack.Screen 
           name='ManageExpense' 
