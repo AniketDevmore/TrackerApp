2127f13181342e0b979bd4e29a8636d9c370892a
Aniket Devmore <aniketdevmore1008@gmail.com>
2024-09-26 22:51:11 +0530

    Added get method

--- src/screens/RecentExpense.tsx
+++ src/screens/RecentExpense.tsx
@@ -1,19 +1,33 @@
 import { Text } from "react-native"
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { getExpenses } from "../util/http";
 
 const RecentExpense = () => {
-    const expenseCtx = useContext(ExpensesContext);
-    const recentExpenses = expenseCtx.expenses.filter((expense:any)=>{
+    // const expenseCtx = useContext(ExpensesContext);
+
+    const [fetchedExpenses, setFetchedExpenses] = useState<any>([]);
+
+    useEffect(() => {
+        const fetchExpenses = async () => {
+          const expenses =  await getExpenses();
+          console.log('expenses---------->>',expenses)
+          setFetchedExpenses(expenses);
+        }
+
+        fetchExpenses();
+    }, []);
+
+    const recentExpenses = fetchedExpenses.filter((expense: any) => {
         const today = new Date();
         const date7DaysAgo = getDateMinusDays(today, 7);
-
-        return (expense.date >= date7DaysAgo) && (expense.data <= today)
+        console.log('today------>>', today, date7DaysAgo)
+        return (expense.date >= date7DaysAgo) && (expense.date <= today)
     })
-    return(
-        <ExpensesOutput expenses={recentExpenses} periodName='Last 7 days' fallbackText={"No expenses registered for last 7 days!"}/>
+    return (
+        <ExpensesOutput expenses={recentExpenses} periodName='Last 7 days' fallbackText={"No expenses registered for last 7 days!"} />
 
     )
 }
--- src/util/http.js
+++ src/util/http.js
@@ -4,3 +4,23 @@ import { url } from "./constants"
 export const storeExpenses = (expenseData) => {
     axios.post(`${url}/expenses.json`, expenseData)
 }
+
+export const getExpenses = async (expenseData) => {
+    const response = await axios.get(`${url}/expenses.json`, expenseData)
+
+    const expenses = [];
+    console.log('response-------------->>', response.data)
+
+    for (const key in response.data) {
+        const expenseObj = {
+            id: key,
+            amount: response.data[key].amount,
+            date: new Date(response.data[key].date),
+            description: response.data[key].description
+
+        }
+        expenses.push(expenseObj)
+    }
+
+    return expenses;
+}
